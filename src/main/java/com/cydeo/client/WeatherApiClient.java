7945e98d3dede1642f9a7e190396fe9e600cb69a
Rukiye Yildirim <rukiyeyildirimca@gmail.com>
2023-10-19 14:33:41 -0700

    Added new validation in the project

---
+++ src/main/java/com/cydeo/client/WeatherApiClient.java
@@ -0,0 +1,19 @@
+package com.cydeo.client;
+
+import com.cydeo.dto.weather.WeatherDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(url = "http://api.weatherstack.com/",name = "WEATHER-CLIENT")
+public interface WeatherApiClient {
+    // http://api.weatherstack.com --> url
+    // /current -->endpoint
+    // ?access_key=87e785a9468054d73eaf989af1e4a99a
+    // &
+    // query=Helsinki
+
+    @GetMapping("/current")
+    WeatherDTO getCurrentWeather(@RequestParam(value = "access_key") String accessKey,
+                                 @RequestParam(value = "query") String city);
+}
